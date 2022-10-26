ffa5b5f635ba82085bb5c6cc360c30c54aa6e0e0
Nightwolf8302 <47755420+Nightwolf8302@users.noreply.github.com>
2022-10-26 19:58:30 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+    <script type="text/javascript">
+
+        function fHallo()
+        {
+         //Ausgabe von Hallo Welt! in ein neues Dokument
+         with(document)
+         {
+         open(); // Öffnet den Fensterinhalt (Dokument) zum Neubeschreiben.
+         write("<title>Hallo Welt in JavaScript<\/title> <h1>Hallo Welt!<\/h1>");
+         close();
+         }
+        }
+
+    </script>
